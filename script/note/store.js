686ec5a6895fdb5cbfad4372e878bf7fd2754b1c
KingDragonLight <15735155629@163.com>
2020-05-21 14:58:40 +0800

    本次修改了script/note/store.js

---
+++ script/note/store.js
@@ -0,0 +1,11 @@
+/**
+ * npm install redux
+ */
+
+// import { createStore } from 'redux'
+
+const reducer={
+    list:['黄河远上白云间','一片孤城万仞山','羌笛何须怨杨柳','春风不度玉门关']
+}
+
+let store = createStore(reducer)
